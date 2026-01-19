f51ec1353a7588ec4e75a1aa87204eb6d77009e6
seungjae <psjsky32@gmail.com>
2026-01-20 08:05:01 +0900

    과제

--- java_exam/src/java_exam/QQQQ.java
+++ java_exam/src/java_exam/QQQQ.java
@@ -4,7 +4,18 @@ public class QQQQ {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for(int i = 2; i<= 100000; i++ ) {
+			
+			int j =2;			
+			for(j = 2; j * j < i && i % j != 0; j++) {
+				
+			}
+			
+			if(j * j > i) {
+				System.out.
+				println(i);
+			}
+		}
 	}
 
 }
