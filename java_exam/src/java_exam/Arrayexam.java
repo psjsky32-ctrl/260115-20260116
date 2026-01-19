5facc4213ae818cf041cb01a8f1cbaa5b2010b63
seungjae <psjsky32@gmail.com>
2026-01-19 17:24:41 +0900

    테스트

--- java_exam/src/java_exam/Arrayexam.java
+++ java_exam/src/java_exam/Arrayexam.java
@@ -12,8 +12,8 @@ public class Arrayexam {
 		
 		
 		int score = 60;
-		System.out.println(score);
-		
+		System.out.println(score);//[I@7c30a502 [I <- 변수 타입 7c~ <-hashcode(변수의 값을 암호화 시킨값)  [ <-변수가 배열이다 I <- 배열을 제외한 타입(int) 
+		//참조형변수
 		//int scoreArray2[] = new int[7];
 	}
 
